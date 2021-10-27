60b62764b29e42b4f27fc62056424f766186e5b5
Robert <robert.rachubinski@gmail.com>
2021-10-27 22:39:50 +0200

    'getting weater information' is ready

--- getting weather information/main.py
+++ getting weather information/main.py
@@ -1,2 +1,13 @@
 import requests
 from pprint import pprint
+
+API_Key = '28195b13606ef59939734f585b66d471'
+
+city = input("Enter a city: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_Key \
+           + "&q=" + city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
