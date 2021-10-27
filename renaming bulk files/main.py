690ed8f9d64cfb08b73b460b1efd16a371a4de14
Robert <robert.rachubinski@gmail.com>
2021-10-27 22:03:28 +0200

    renaming file works

--- renaming bulk files/main.py
+++ renaming bulk files/main.py
@@ -1,1 +1,18 @@
-dsf
+import os
+
+
+def rename_files():
+    i = 0
+    path = "/home/robert/Six_Quick_Python_Projects/renaming bulk files/" \
+           "files_for_test/"
+    for filename in os.listdir(path):
+        old_file_name = path + filename
+        print('\nold:', old_file_name)
+        new_file_name = path + "exfile_" + str(i)
+        print('new:', new_file_name)
+        os.rename(old_file_name, new_file_name)
+        i += 1
+
+
+if __name__ == '__main__':
+    rename_files()
