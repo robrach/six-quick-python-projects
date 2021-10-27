44d792b5ed74d4d480100efa1a200c28a8fe31b9
Robert <robert.rachubinski@gmail.com>
2021-10-27 21:15:47 +0200

    Done: scraper gives the link to user's avatar

--- web scraping/main.py
+++ web scraping/main.py
@@ -5,4 +5,5 @@ github_user = input('Input Github user name: ')
 url = 'https://github.com/'+github_user
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
-print(soup)
+profile_image_link = soup.find('img', {'alt': 'Avatar'})['src']
+print(profile_image_link)
