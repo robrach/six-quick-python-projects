faac13bd01294e50c3d8324b04ffc5899311813c
Robert <robert.rachubinski@gmail.com>
2021-10-28 21:33:01 +0200

    Refactor in password generator

--- 5 - password generator/main.py
+++ 5 - password generator/main.py
@@ -1,23 +1,27 @@
 import random
 
-print('Password Generator')
 
-chars = 'qwertyuiopasdfghjklzxcvbnm' \
+def password_generator(amount_of_passwords, length_of_password):
+    amount_of_passwords = int(amount_of_passwords)
+    length_of_password = int(length_of_password)
+
+    chars = 'qwertyuiopasdfghjklzxcvbnm' \
         'QWERTYUIOPASDFGHJKLZXCVBNM' \
         '1234567890' \
         '!@#$%^&*()' \
         '-_=+[{]}'";:\|,<.>/?"
 
-amount_of_passwords = input('How many passwords to generate: ')
-amount_of_passwords = int(amount_of_passwords)
+    print('\nGenerated passwords:')
 
-length_of_password = input('Password length: ')
-length_of_password = int(length_of_password)
+    for password_being_generated in range(amount_of_passwords):
+        password = ''
+        for position in range(length_of_password):
+                password += random.choice(chars)
+        print(password)
 
-print('\nGenerated passwords:')
 
-for pwd in range(amount_of_passwords):
-    passwords = ''
-    for c in range(length_of_password):
-        passwords += random.choice(chars)
-    print(passwords)
+if __name__ == '__main__':
+    print('Password Generator')
+    amount_of_passwords = input('How many passwords to generate: ')
+    length_of_password = input('Password length: ')
+    password_generator(amount_of_passwords, length_of_password)
